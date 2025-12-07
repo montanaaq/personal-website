fd0bc9963940b056165fc4a3c93f2b7734830723
Montana <a.presovsky@list.ru>
2025-12-07 20:57:24 +0300

    fixed for build

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,19 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'esbuild',
+    minify: false,
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'motion-vendor': ['motion/react'],
-          'ogl-vendor': ['ogl'] // Separate OGL into its own chunk
-        }
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       }
     },
     chunkSizeWarningLimit: 1000,
     cssCodeSplit: true,
     sourcemap: false
   },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom', 'motion/react', 'ogl'],
-  },
+
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   }
