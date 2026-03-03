1827895c9b7a2398748ef46b1e0b9f7ac39df439
Montana <a.presovsky@list.ru>
2026-03-03 13:11:25 +0300

    changed: logic for circles background on desktop and mobile

--- src/app/screens/home-page/App.tsx
+++ src/app/screens/home-page/App.tsx
@@ -1,27 +1,13 @@
 import type { FC } from 'react'
 import Circles from '@/components/ui/backgrounds/Circles/Circles.tsx'
-import useIsChristmas from '@/shared/hooks/useIsChristmas.ts'
-import useIsDesktop from '@/shared/hooks/useIsDesktop.ts'
-import ViewPage from './ViewPage.tsx'
+import ViewPage from './ViewPage'
 
 const App: FC = () => {
-  const isDesktop = useIsDesktop()
-  const isChristmas = useIsChristmas()
-  const showCircles = !isDesktop && !isChristmas
-
   return (
     <div className="App">
-      {isDesktop ? (
-        <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
-          <ViewPage />
-        </div>
-      ) : showCircles ? (
-        <Circles>
-          <ViewPage />
-        </Circles>
-      ) : (
+      <Circles>
         <ViewPage />
-      )}
+      </Circles>
     </div>
   )
 }
